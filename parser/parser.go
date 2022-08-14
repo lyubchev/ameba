2a8d9d699ad852b6e6cb084b6a45edc744215d56
Lyubo Lyubchev <lyubo_2317@abv.bg>
2022-08-14 22:43:10 +0300

    tests, remove nextToken usage in New()

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,93 @@
 package parser
+
+import (
+	"github.com/impzero/ameba/ast"
+	"github.com/impzero/ameba/lexer"
+	"github.com/impzero/ameba/token"
+)
+
+type Parser struct {
+	lex *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+
+	errors []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		lex:    l,
+		errors: []string{},
+	}
+
+	p.curToken = p.lex.NextToken()
+	p.peekToken = p.lex.NextToken()
+	return p
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) expectPeek(tok token.TokenType) bool {
+	if p.peekTokenIs(tok) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) peekTokenIs(tok token.TokenType) bool {
+	return p.peekToken.Type == tok
+}
+
+func (p *Parser) curTokenIs(tok token.TokenType) bool {
+	return p.curToken.Type == tok
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lex.NextToken()
+}
