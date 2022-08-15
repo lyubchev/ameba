5dd428335c1c1e8daaa31a91a9e2646e2e5040a7
Lyubo Lyubchev <lyubo_2317@abv.bg>
2022-08-16 01:54:45 +0300

    parse return statement (partially xd)

--- parser/parser.go
+++ parser/parser.go
@@ -55,6 +55,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -81,6 +83,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+// expectPeek validates if token is of the same type as the next token and progresses to next token by calling nextToken()
 func (p *Parser) expectPeek(tok token.TokenType) bool {
 	if p.peekTokenIs(tok) {
 		p.nextToken()
